e75f81861099a8ac08e922d4c71d13f29e30e945
Mrinmoy Koiri <153649837+MK10UNoY@users.noreply.github.com>
2024-12-23 20:45:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,11 +9,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('templates/index.html')
+    return render_template('./templates/index.html')
 
 @app.route('/register_user')
 def register():
-    return render_template('register_user.html')
+    return render_template('./templates/register_user.html')
 
 if __name__ == '__main__':
     app.run()
